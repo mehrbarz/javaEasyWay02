9313c350efdedface66e863e7724d07f20eabef9
mehrbarz <mehrbarzegar@gmail.com>
2021-03-25 19:56:46 +0100

    Hi-Lo guessing game for a single full round of play.

--- src/hilo02/Hilo02.java
+++ src/hilo02/Hilo02.java
@@ -7,12 +7,19 @@ public class Hilo02 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
 		int randomized = (int)(Math.random()*100 +1);
-		System.out.println(randomized);
+		 //System.out.println(randomized);
 		int guess = 0;
-		System.out.println("Guess a number between 1 and 100 : ");
-		guess = scan.nextInt();
-		System.out.println("You entered "+ guess + ".");
-		
+		while (guess != randomized){
+			System.out.println("Guess a number between 1 and 100 : ");
+			guess = scan.nextInt();
+		//	System.out.println("You entered "+ guess + ".");
+			if(guess< randomized)
+				System.out.println( guess + " is too low. Try again.");
+			else if (guess > randomized)
+				System.out.println(guess + " is too high. Try again. ");
+			else
+				System.out.println(guess + " is correct. You win! ");
+		}
 
 	}
 
