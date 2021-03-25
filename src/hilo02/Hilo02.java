125b736b04667cca74a24c5663f442d96edc2178
mehrbarz <mehrbarzegar@gmail.com>
2021-03-25 19:36:09 +0100

    get the user's guess

--- src/hilo02/Hilo02.java
+++ src/hilo02/Hilo02.java
@@ -1,10 +1,18 @@
 package hilo02;
 
+import java.util.Scanner;
+
 public class Hilo02 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
 		int randomized = (int)(Math.random()*100 +1);
 		System.out.println(randomized);
+		int guess = 0;
+		System.out.println("Guess a number between 1 and 100 : ");
+		guess = scan.nextInt();
+		System.out.println("You entered "+ guess + ".");
+		
 
 	}
 
